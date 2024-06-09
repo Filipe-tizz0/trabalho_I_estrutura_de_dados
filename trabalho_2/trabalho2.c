1ccb3b7397ade2404dcdc5ca4fdb401a15c9eb3e
Filipe <filipetizzo@gmail.com>
2024-06-09 08:57:46 -0300

    upload inicial

---
+++ trabalho_2/trabalho2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int p1,atividade;
+    float media_final, p2, trabalho;
+    char area[10], nome[20];
+    
+    media_final = (p2+p1+atividade+trabalho)/4;
+    
+    printf("media final do aluno: ");
+
+    return 0;
+}
